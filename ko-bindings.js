04f30265ad9ba9bf62cc06b779f2eb5dadda803f
chernyee <chernyee@msn.com>
2015-04-28 15:18:19 +0800

    Create ko-bindings.js

---
+++ ko-bindings.js
@@ -0,0 +1,15 @@
+(function(ko) {
+  ko.bindingHandlers['reference'] = {
+      update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+          var options = ko.unwrap(valueAccessor());
+          var data = ko.unwrap(options.options);
+          var value = ko.unwrap(options.value);
+          for (var i = 0, description = undefined; i < data.length; ++i) {
+              if (ko.unwrap(data[i][options.key || 'id']) == value) {
+                  description = ko.unwrap(data[i][options.caption || 'description']);
+              }
+          }
+          element.innerHTML = description || '';
+      }
+  };
+})(ko);
